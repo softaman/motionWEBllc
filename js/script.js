20cbc990562769249ee9800d79e7f304fc44c5fa
Aman Ismailov <aman2021js@gmail.com>
2021-10-11 18:05:16 +0600

    initial

--- js/script.js
+++ js/script.js
@@ -50,7 +50,14 @@ window.onclick = function (event) {
     }
 }
 
-
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+}
 
 
 
